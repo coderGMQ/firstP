4603bbef32d02ef14bd41b6edff89f05ec448ea8
甘明强 <ganmingqiang@ganmingqiangdeMacBook-Air.local>
2018-11-08 23:47:19 +0800

    chushihua

---
+++ oneone/oneone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  oneone
+//
+//  Created by 甘明强 on 2018/11/8.
+//  Copyright © 2018年 zywulian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ oneone/oneone/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  oneone
+//
+//  Created by 甘明强 on 2018/11/8.
+//  Copyright © 2018年 zywulian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+  
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
